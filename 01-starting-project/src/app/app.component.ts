d856775c5e7c3886581f870d8e45f7354903c584
julienF <julien.francois@altametris.com>
2026-05-04 17:11:34 +0200

    section 2, module 32

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from '../utils/dummy-users';
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users: User[] = DUMMY_USERS;
+}
