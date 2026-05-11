68f8d768ce52db728464daadb427b329c2d8f0af
julienF <julien.francois@altametris.com>
2026-05-11 12:16:10 +0200

    adding new component tasks

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from '../utils/dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
+import { find } from 'rxjs';
 
 type User = {
   id: string;
@@ -13,14 +15,19 @@ type User = {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users: User[] = DUMMY_USERS;
+  protected userIdSelected: string = '';
+
+  get userNameSelected() {
+    return this.users.find(user => user.id === this.userIdSelected)?.name;
+  }
 
   onSelectUser(id: string) {
-    console.log('Selected user with id:', id);
+    this.userIdSelected = id;
   }
 }
