213aa34f4be0b5631b4254f2f81412dd7c2f08b0
julienF <julien.francois@altametris.com>
2026-05-21 17:16:01 +0200

    start project

--- 02-starting-project/src/app/app.component.ts
+++ 02-starting-project/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
+import { TableComponent } from './table/table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [HeaderComponent, FormComponent, TableComponent]
 })
 export class AppComponent {}
