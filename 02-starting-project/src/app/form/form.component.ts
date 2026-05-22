581d2d6264681c4b54555afe496fa7f99469fe9f
julienF <julien.francois@altametris.com>
2026-05-22 14:53:44 +0200

    commit before upgrade version ng

--- 02-starting-project/src/app/form/form.component.ts
+++ 02-starting-project/src/app/form/form.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Investment } from '../../utils/types/investment';
+import {form, FormField} from '@angular/forms/signals';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [FormField],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
+  investmentModel = signal<Investment>({
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expectedReturn: 0,
+    duration: 0
+  });
 
+  investmentForm = form()
+
+  onSubmit() {
+    console.log(this.newInvestment());
+  }
 }
---
+++ 02-starting-project/src/utils/types/investment.ts
@@ -0,0 +1,6 @@
+export type Investment = {
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+}
